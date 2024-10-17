532bdfef17764d8a5770de75b5ee1825684c75be
creepersaur <69436784+creepersaur@users.noreply.github.com>
2024-10-17 14:05:55 +0530

    Docs > New - fixed entries

--- src/shared/Quark/Entries/init.lua
+++ src/shared/Quark/Entries/init.lua
@@ -3,13 +3,21 @@ local Entries = {
 	Created = {}
 }
 
-function Entries.new(n: number, func: (i: number) -> Instance | Types.New)
+function Entries.new(n: number | {unknown}, func: (i: number, v: unknown?) -> Instance | Types.New): {Instance | Types.New}
 	local children = {}
 	
-	for i = 1, n do
-		local object = func(i)
-		table.insert(children, object)
-		table.insert(Entries.Created, object)
+	if typeof(n) == "number" then
+		for i = 1, n do
+			local object = func(i)
+			table.insert(children, object)
+			table.insert(Entries.Created, object)
+		end
+	elseif typeof(n) == "table" then
+		for i,v in n do
+			local object = func(i,v)
+			table.insert(children, object)
+			table.insert(Entries.Created, object)
+		end
 	end
 
 	return children
